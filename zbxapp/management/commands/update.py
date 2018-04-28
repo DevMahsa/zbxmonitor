3c6b8d706a4d07294feb9107062ae8ab1a8962f0
=Mahsa <mahsa.gol89@gmail.com>
2018-04-28 11:36:05 +0430

    first!

--- zbxapp/management/commands/update.py
+++ zbxapp/management/commands/update.py
@@ -4,13 +4,16 @@ from pyzabbix import ZabbixAPI
 from zbxapp.models import Name
 
 def update():
-    zapi = ZabbixAPI("http://192.168.112.157:4720")
-    zapi.login("Mahsa", "Mahsa871^")
-    return zapi
-
-    for h in zapi.host.get(output="extend"):
-        get = Name(server=h['name'], date = datetime.now())
-        get.save()
+    try:
+        zapi = ZabbixAPI("http://192.168.112.157:4720")
+        zapi.login("Mahsa", "Mahsa871^")
+        for h in zapi.host.get(output="extend"):
+            if not Name.objects.filter(server = h['name']).exists():
+                get = Name(server=h['name'])
+                get.save()
+        print 'complete'
+    except Exception as e:
+        print e
 
 
 class Command(BaseCommand):
